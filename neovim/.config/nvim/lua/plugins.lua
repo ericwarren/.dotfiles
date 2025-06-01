03da4317407735d4a12ead2373df0dafed01c7a3
Eric Warren <eric.warren7@gmail.com>
2025-06-01 03:55:25 -0500

    dasfas

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -46,7 +46,7 @@ return require('lazy').setup({
         capabilities = capabilities,
       })
       lspconfig.omnisharp.setup({
-        cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+        cmd = { vim.fn.stdpath("data") .. "/mason/bin/omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
         capabilities = capabilities,
         enable_roslyn_analyzers = true,
         organize_imports_on_format = true,
